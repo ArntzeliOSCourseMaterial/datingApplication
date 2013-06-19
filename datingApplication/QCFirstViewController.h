dafdda797f590deef55009ea1d62cb000ff55e17
Eliot Arntz <arntzel@gmail.com>
2013-06-18 20:52:19 -0400

    Initial Commit

---
+++ datingApplication/QCFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  QCFirstViewController.h
+//  datingApplication
+//
+//  Created by Eliot Arntz on 6/18/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCFirstViewController : UIViewController
+
+@end
---
+++ datingApplication/QCSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  QCSecondViewController.h
+//  datingApplication
+//
+//  Created by Eliot Arntz on 6/18/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCSecondViewController : UIViewController
+
+@end
